e1dc44d36555a348247a299422a1f4831a0bfc65
ShlokD <shlokpdesai@gmail.com>
2018-04-29 13:52:39 -0700

    Clean

--- js/utils.js
+++
@@ -1,8 +0,0 @@
-define(() => {
-	const cloneArray = arr => arr.map(childArr => childArr.slice());
-	const flattenArray = arr => [].concat.apply([], arr);
-	return {
-		cloneArray,
-		flattenArray
-	};
-});
